873bcb03070f5554688a2b81fec822f9b623d5b1
Shihara Jayasinghe <56840369+shiharJ@users.noreply.github.com>
2022-11-16 21:33:31 +0530

    Push Frontend

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+    return (
+        <div>
+            <footer className = "footer">
+                 <span className="text-muted">EAD group project - UOM</span>
+            </footer>
+        </div>
+    )
+}
+
+export default FooterComponent
